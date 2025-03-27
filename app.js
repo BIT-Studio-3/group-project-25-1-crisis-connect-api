ae36c73856ff1de427f7f2c04f29bee8a70ae94d
Joanna Marowa <marojm1@student.op.ac.nz>
2025-03-28 10:39:30 +1300

    feat(js) : add swagger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import the Express module
 import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 // Import the index routes module
 import indexRoutes from "./routes/index.js";
@@ -11,7 +13,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Use the routes module
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Student Management System API",
+            version: "1.0.0",
+            description: "A student management system API",
+            contact: {
+                name: "Joanna Marowa",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/v1/*.js"],
+};
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use("/", indexRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Start the server on port 3000
 app.listen(PORT, () => {
