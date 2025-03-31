6f581056692d614a7fd4dffc4e145b5f98c04e92
Joanna Marowa <marojm1@student.op.ac.nz>
2025-03-31 15:34:54 +1300

    feat: (js) : update createDamage ()

--- controllers/v1/damage.js
+++ controllers/v1/damage.js
@@ -12,6 +12,20 @@ const createDamage = async (req,res) => {
                 description : req.body.description,      
                      
             } 
-        })
-    }
-}
+        });
+
+        // Get all damage from the damage table 
+        const newDamage = await prisma.hazard.findMany();
+
+        //send JSON response
+
+        return res.status(201).json({
+            message: "Institution successfully created",
+            data: newInstitutions,
+          });
+        } catch (err) {
+          return res.status(500).json({
+            message: err.message,
+          });
+        }
+      };
