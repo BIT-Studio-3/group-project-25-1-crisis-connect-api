5b8c543380334f84fdc95e021f178b43c089a8e9
Mustafa12315 <HABIM3@student.op.ac.nz>
2025-03-31 16:50:47 +1300

    feat(JS): Added the code for creating a new hazard record

--- controllers/v1/hazard.js
+++ controllers/v1/hazard.js
@@ -3,3 +3,27 @@ import prisma from "../../prisma/client.js";
 
 
 
+const createHazard = async (req, res) => {
+    try {
+      await prisma.hazard.create({
+        data: {
+          streetNumber: req.body.streetNumber,
+          streetName: req.body.streetName,
+          city: req.body.city,
+          region: req.body.region,
+          type: req.body.type,
+        },
+      });
+  
+      const newHazards = await prisma.hazard.findMany();
+  
+      return res.status(201).json({
+        message: "Hazard successfully recorded",
+        data: newHazards,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: err.message,
+      });
+    }
+};
