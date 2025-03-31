18339bf6832b7fc06cc2e5819fb513a1063e41c1
Joanna Marowa <marojm1@student.op.ac.nz>
2025-03-31 16:03:51 +1300

    feat(js): add damage router

---
+++ routes/damage.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+        recordDamage ,
+        getDamages,
+        getDamage ,
+        updateDamage,
+        deleteDamage,
+} from "../controllers/v1/damage.js";
+
+const router = express.Router();
+
+router.post("/", recordDamage);
+router.get("/", getDamages);
+router.get("/:id", getDamage);
+router.put("/:id", updateDamage);
+router.delete("/:id", deleteDamage);
+
+
+export default router;
