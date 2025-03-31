bdcfe79a43ba2af2d15bce5360959ae777a2f5e2
Mustafa12315 <HABIM3@student.op.ac.nz>
2025-03-31 16:47:10 +1300

    feat(JS): Added routes path and  type for applicationJSON

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-// Import the Express module
 import express from "express";
 import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-
+ 
 // Import the index routes module
 import indexRoutes from "./routes/index.js";
-
+import hazardRoutes from "./routes/v1/hazard.js";
+import { isContentTypeApplicationJSON } from "./middleware/utils.js";
+ 
 // Create an Express application
 const app = express();
-
+ 
 // Use the PORT environment variable or 3000
 const PORT = process.env.PORT || 3000;
-
+ 
 // Use the routes module
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -35,15 +36,17 @@ const swaggerOptions = {
     apis: ["./routes/v1/*.js"],
 };
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use(isContentTypeApplicationJSON);
 app.use("/", indexRoutes);
+app.use("/routes/hazard", hazardRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
+ 
 // Start the server on port 3000
 app.listen(PORT, () => {
   console.log(
     `Server is listening on port ${PORT}. Visit http://localhost:${PORT}`
   );
 });
-
+ 
 // Export the Express application. May be used by other modules. For example, API testing
 export default app;
