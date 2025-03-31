541e6348619e70f60fda33dc93c4d3e6c275e9c4
marojm1 <MAROJM1@student.op.ac.nz>
2025-04-01 11:05:04 +1300

    feat(js) : implement damage repository in controller

--- controllers/v1/damage.js
+++ controllers/v1/damage.js
@@ -2,146 +2,145 @@
  * @file Manages all  damage methods
  * @author Joanna Marowa
  */
-import prisma from "../../prisma/client.js";
 
-const recordDamage = async (req,res) => {
-    try{
-        await prisma.damage.create({
-            data: {
-                streetNumber:req.body.streetNumber, 
-                streetName:req.body.streetName,   
-                city : req.body.City,           
-                region  : req.body.Region,       
-                type  : req.body.type,       
-                description : req.body.description,      
-                     
-            } 
-        });
+import damageRepository from "../../repositories/damage.js";
 
-        // Get all damage from the damage table 
-        const newDamage = await prisma.hazard.findMany();
+const recordDamage = async (req, res) => {
+  try {
+    await damagerepository.create({
+      data: {
+        streetNumber: req.body.streetNumber,
+        streetName: req.body.streetName,
+        city: req.body.City,
+        region: req.body.Region,
+        type: req.body.type,
+        description: req.body.description,
 
-        //send JSON response
+      }
+    });
 
-        return res.status(201).json({
-            message: "Damage successfully recorded",
-            data: newDamage,
-          });
-        } catch (err) {
-          return res.status(500).json({
-            message: err.message,
-          });
-        }
-      };
+    // Get all damage from the damage table 
+    const newDamage = await Damagerepository.findMany();
 
-      const getDamages = async (req, res) => {
-        try {
-          const damage  = await prisma.damage.findMany();
-      
-          // Check if there are no institutions
-          if (!damage) {
-            return res.status(404).json({ message: "No damage record found" });
-          }
-      
-          return res.status(200).json({
-            data: damage,
-          });
-        } catch (err) {
-          return res.status(500).json({
-            message: err.message,
-          });
-        }
-      };
+    //send JSON response
+    return res.status(201).json({
+      message: "Damage successfully recorded",
+      data: newDamage,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
-      const getDamage = async (req, res) => {
-        try {
-          const damage  = await prisma.damage.findUnique({
-            where: { id: req.params.id },
-          });
-      
-          // Check if there is no institution
-          if (!damage) {
-            return res.status(404).json({
-              message: `No damage record with the id: ${req.params.id} found`,
-            });
-          }
-      
-          return res.status(200).json({
-            data: damage,
-          });
-        } catch (err) {
-          return res.status(500).json({
-            message: err.message,
-          });
-        }
-      };
+const getDamages = async (req, res) => {
+  try {
+    const damage = await damageRepository.findMany();
+
+    // Check if there are no institutions
+    if (!damage) {
+      return res.status(404).json({ message: "No damage record found" });
+    }
+
+    return res.status(200).json({
+      data: damage,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getDamage = async (req, res) => {
+  try {
+    const damage = await damageRepository.findUnique({
+      where: { id: req.params.id },
+    });
+
+    // Check if there is no institution
+    if (!damage) {
+      return res.status(404).json({
+        message: `No damage record with the id: ${req.params.id} found`,
+      });
+    }
+
+    return res.status(200).json({
+      data: damage,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 const updateDamage = async (req, res) => {
-    try {
-      // Find the institution by id
-      let damage= await prisma.damage.findUnique({
-        where: { id: req.params.id },
-      });
-  
-      // Check if there is no institution
-      if (!damage) {
-        return res.status(404).json({
-          message: `No institution with the id: ${req.params.id} found`,
-        });
-      }
-  
-      // Update the institution
-      damage = await prisma.damage.update({
-        where: { id: req.params.id },
-        data: {
-          // Data to be updated     
-                type  : req.body.type,       
-                description : req.body.description, 
-        },
-      });
-  
-      return res.status(200).json({
-        message: `Damage record with the id: ${req.params.id} successfully updated`,
-        data: institution,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        message: err.message,
+  try {
+    // Find the institution by id
+    let damage = await damageRepository.findUnique({
+      where: { id: req.params.id },
+    });
+
+    // Check if there is no institution
+    if (!damage) {
+      return res.status(404).json({
+        message: `No institution with the id: ${req.params.id} found`,
       });
     }
-  };
 
-  const deleteDamage = async (req, res) => {
-    try {
-      const damage = await prisma.damage.findUnique({
-        where: { id: req.params.id },
-      });
-  
-      if (!damage) {
-        return res.status(404).json({
-          message: `No damage with the id: ${req.params.id} found`,
-        });
-      }
-  
-      await prisma.damage.delete({
-        where: { id: req.params.id },
-      });
-  
-      return res.json({
-        message: `Damage record with the id: ${req.params.id} successfully deleted`,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        message: err.message,
+    // Update the institution
+    damage = await damageRepository.update({
+      where: { id: req.params.id },
+      data: {
+        // Data to be updated     
+        type: req.body.type,
+        description: req.body.description,
+      },
+    });
+
+    return res.status(200).json({
+      message: `Damage record with the id: ${req.params.id} successfully updated`,
+      data: institution,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const deleteDamage = async (req, res) => {
+  try {
+    const damage = await damageRepository.findUnique({
+      where: { id: req.params.id },
+    });
+
+    if (!damage) {
+      return res.status(404).json({
+        message: `No damage with the id: ${req.params.id} found`,
       });
     }
-  };
 
-  export {
-    recordDamage ,
-    getDamages,
-    getDamage ,
-    updateDamage,
-    deleteDamage,
-  };
-  
+    await damageRepository.delete({
+      where: { id: req.params.id },
+    });
+
+    return res.json({
+      message: `Damage record with the id: ${req.params.id} successfully deleted`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+export {
+  recordDamage,
+  getDamages,
+  getDamage,
+  updateDamage,
+  deleteDamage,
+};
