520c7cb4e5243b5a560c37b2339a3bd745bb8ff6
Mustafa12315 <HABIM3@student.op.ac.nz>
2025-04-01 17:45:07 +1300

    feat(js): Validation for Posting new hazard for StreetName

--- middleware/validation/hazard.js
+++ middleware/validation/hazard.js
@@ -1,2 +1,12 @@
 import Joi from "joi";
 
+// Validation for creating a new hazard
+const validatePostHazard = (req, res, next) => {
+  const hazardSchema = Joi.object({
+    streetName: Joi.string().min(3).max(100).required().messages({
+      "string.base": "streetName should be a string",
+      "string.empty": "streetName cannot be empty",
+      "string.min": "streetName should have a minimum length of {#limit}",
+      "string.max": "streetName should have a maximum length of {#limit}",
+      "any.required": "streetName is required",
+    }),
