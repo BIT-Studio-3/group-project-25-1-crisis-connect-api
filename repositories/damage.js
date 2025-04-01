efab79fbe1d29d49f45032e873d57e183704afc9
marojm1 <MAROJM1@student.op.ac.nz>
2025-04-02 09:13:29 +1300

    feat(js) :  damage filter

--- repositories/damage.js
+++ repositories/damage.js
@@ -5,10 +5,22 @@ class DamageRepository {
     return await prisma.damage.create({ data });
   }
 
-  async findAll() {
-    return await prisma.damage.findMany();
-  }
+  async findAll(filters = {}) {
+    // Create an empty query object
+    const query = {};
 
+    if (Object.keys(filters).length > 0) {
+      query.where = {};
+      // Loop through the filters and apply them dynamically
+      for (const [key, value] of Object.entries(filters)) {
+        if (value) {
+          query.where[key] = { contains: value };
+        }
+      }
+    }
+
+    return await prisma.damage.findMany(query);
+}
   async findById(id) {
     return await prisma.damage.findUnique({
       where: { id },
