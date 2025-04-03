c55a50eea0b300545118d8fe0796b0f47bcb0f7f
Joanna Marowa <marojm1@student.op.ac.nz>
2025-04-03 14:34:02 +1300

    refactor(prisma) : prisma migration

--- prisma/migrations/20250402235652_/migration.sql
+++ prisma/migrations/20250402235652_/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Institution` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Institution";
-
 -- CreateTable
 CREATE TABLE "Damage" (
     "id" TEXT NOT NULL,
