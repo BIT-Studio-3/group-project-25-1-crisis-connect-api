4ec4616ad5e8a5802495a7c168484857802b8fbf
Mustafa12315 <HABIM3@student.op.ac.nz>
2025-05-30 11:13:34 +1200

    fix(JSON):Added the migration and removed the duplicated jsonwebtoken

--- prisma/migrations/20250529231217_/migration.sql
+++ prisma/migrations/20250529231217_/migration.sql
@@ -1,3 +1,22 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('FENZ', 'civilDefence', 'Police', 'NEMA');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "emailAddress" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "loginAttempts" INTEGER NOT NULL DEFAULT 0,
+    "lastLoginAttempt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "Damage" (
     "id" TEXT NOT NULL,
@@ -27,3 +46,6 @@ CREATE TABLE "hazard" (
 
     CONSTRAINT "hazard_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_emailAddress_key" ON "User"("emailAddress");
