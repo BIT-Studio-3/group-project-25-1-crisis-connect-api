c04825d2630d07b8b4f29cfd7bd3bb82076943b6
Joanna Marowa <marojm1@student.op.ac.nz>
2025-05-30 09:21:41 +1200

    chore : migrating

---
+++ prisma/migrations/20250529211214_/migration.sql
@@ -0,0 +1,49 @@
+-- CreateTable
+CREATE TABLE "Damage" (
+    "id" TEXT NOT NULL,
+    "streetNumber" TEXT NOT NULL,
+    "streetName" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "region" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Damage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "hazard" (
+    "id" TEXT NOT NULL,
+    "streetNumber" TEXT NOT NULL,
+    "streetName" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "region" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "hazard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "task" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "requirements" TEXT NOT NULL,
+    "urgency" TEXT NOT NULL,
+    "resources" TEXT NOT NULL,
+    "assignedTo" TEXT NOT NULL,
+    "supervisor" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "priority" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deadline" TIMESTAMP(3),
+    "completedAt" TIMESTAMP(3),
+    "department" TEXT NOT NULL,
+
+    CONSTRAINT "task_pkey" PRIMARY KEY ("id")
+);
