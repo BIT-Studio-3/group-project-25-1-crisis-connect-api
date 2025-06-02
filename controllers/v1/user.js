3b9e5f6bcadd3178bd0f6b3b91f4b818e9bfea2e
Joanna Marowa <marojm1@student.op.ac.nz>
2025-06-02 14:12:30 +1200

    fix(js) : fix user Get issue

--- controllers/v1/user.js
+++ controllers/v1/user.js
@@ -1,116 +1,93 @@
 import GenericRepository from "../../repositories/generic.js";
 
-// Instantiate the GenericRepository with the 'hazard' model
-const userRepository = new GenericRepository('user'); 
+// Instantiate the GenericRepository with the 'user' model
+const userRepository = new GenericRepository('user');
 
 const createUser = async (req, res) => {
-  // Try/catch blocks are used to handle exceptions
   try {
-    // Create a new institution
     await userRepository.create({
-      // Data to be inserted
       data: {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         role: req.body.role,
-        email: req.body.email,
+        emailAddress: req.body.emailAddress,
       },
     });
 
-    // Get all institutions from the institution table
     const newUser = await userRepository.findAll();
 
-    // Send a JSON response
     return res.status(201).json({
       message: "User successfully created",
       data: newUser,
     });
   } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
+    return res.status(500).json({ message: err.message });
   }
 };
-// Add the following code under the createInstitution function
+
 const getUsers = async (req, res) => {
   try {
-    const user = await userRepository.findAll();
+    const users = await userRepository.findAll();
 
-    // Check if there are no institutions
-    if (!user) {
+    if (!users || users.length === 0) {
       return res.status(404).json({ message: "No users found" });
     }
 
-    return res.status(200).json({
-      data: user,
-    });
+    return res.status(200).json({ data: users });
   } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
+    return res.status(500).json({ message: err.message });
   }
 };
-// Add the following code under the getInstitutions function
+
 const getUser = async (req, res) => {
   try {
     const user = await userRepository.findUnique({
       where: { id: req.params.id },
     });
 
-    // Check if there is no institution
     if (!user) {
       return res.status(404).json({
         message: `No user with the id: ${req.params.id} found`,
       });
     }
 
-    return res.status(200).json({
-      data: user,
-    });
+    return res.status(200).json({ data: user });
   } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
+    return res.status(500).json({ message: err.message });
   }
 };
-// Add the following code under the getInstitution function
+
 const updateUser = async (req, res) => {
   try {
-    // Find the institution by id
     let user = await userRepository.findUnique({
       where: { id: req.params.id },
     });
 
-    // Check if there is no institution
     if (!user) {
       return res.status(404).json({
         message: `No user with the id: ${req.params.id} found`,
       });
     }
 
-    // Update the institution
     user = await userRepository.update({
       where: { id: req.params.id },
       data: {
-        // Data to be updated
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         role: req.body.role,
-        email: req.body.email,
+        emailAddress: req.body.emailAddress,
       },
     });
 
     return res.status(200).json({
-      message: `Institution with the id: ${req.params.id} successfully updated`,
+      message: `User with the id: ${req.params.id} successfully updated`,
       data: user,
     });
   } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
+    return res.status(500).json({ message: err.message });
   }
 };
-// Add the following code under the updateInstitution function
+
 const deleteUser = async (req, res) => {
   try {
     const user = await userRepository.findUnique({
@@ -119,7 +96,7 @@ const deleteUser = async (req, res) => {
 
     if (!user) {
       return res.status(404).json({
-        message: `No institution with the id: ${req.params.id} found`,
+        message: `No user with the id: ${req.params.id} found`,
       });
     }
 
@@ -131,12 +108,10 @@ const deleteUser = async (req, res) => {
       message: `User with the id: ${req.params.id} successfully deleted`,
     });
   } catch (err) {
-    return res.status(500).json({
-      message: err.message,
-    });
+    return res.status(500).json({ message: err.message });
   }
 };
-// Add the following code under the deleteInstitution function
+
 export {
   createUser,
   getUsers,
